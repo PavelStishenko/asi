17923eca2dfcf1ef8bfea519cbf4a77819d6dd00
Pavel V. Stishenko <pstishenko@gmail.com>
2022-04-18 16:53:12 +0100

    --allow-run-as-root

--- tests/testcases/test_esp.aims/run.sh
+++ tests/testcases/test_esp.aims/run.sh
@@ -6,12 +6,12 @@ mkdir work
 cd work
 
 TEST_LOG=test1
-mpiexec -n 1 $TESTS/test_esp-aims.x | tee  $TEST_LOG
+mpiexec --allow-run-as-root -n 1 $TESTS/test_esp-aims.x | tee  $TEST_LOG
 diff --color -s $TEST_LOG ../$TEST_LOG
 
 
 TEST_LOG=test2
-mpiexec -n 2 $TESTS/test_esp-aims.x | tee  $TEST_LOG
+mpiexec --allow-run-as-root -n 2 $TESTS/test_esp-aims.x | tee  $TEST_LOG
 diff --color -s $TEST_LOG ../$TEST_LOG
 
 
