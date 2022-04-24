07f7fdf302bb23f2106626ff6906f93ee023905d
Pavel V. Stishenko <pstishenko@gmail.com>
2022-04-24 21:37:40 +0100

    added ase/mpi4py in python installation

--- tests/testcases/run_aims_tests.sh
+++ tests/testcases/run_aims_tests.sh
@@ -5,8 +5,8 @@ ulimit -s unlimited
 
 export TESTS="${TESTS:-${PWD}/../build/}"
 export MPIEXEC="${MPIEXEC:-mpiexec}"
-export PYTHONPATH=$PYTHONPATH:${PWD}/../../python
-export TESTING_PYTHON="${TESTING_PYTHON:-${HOME}/ve/bin/python3}"
+export TESTING_PYTHON="${TESTING_PYTHON:-python3}"
+#export PYTHONPATH=$PYTHONPATH:${PWD}/../../python
 #export DMPREDICT_PATH=${PWD}/../../../dmpredict/
 #export PYTHONPATH=$PYTHONPATH:${DMPREDICT_PATH}
 
