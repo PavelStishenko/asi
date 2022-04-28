95d3efe7d496fd423505cc0b82f40fc1439a7990
Pavel V. Stishenko <pstishenko@gmail.com>
2022-04-28 14:59:24 +0100

    Added API documentation

--- include/asi.h
+++ include/asi.h
@@ -61,7 +61,7 @@ extern "C" void ASI_init(const char *inputpath, const char *outputfilename, int 
 /*!
   Set coordinates of atoms
   
-  Set coordinates of atoms.
+  Set coordinates of atoms. Coordinates are copied into interal storage, so the pointer do not have to be valid after return.
   \param coords pointer to array in row major format with shape [n_atoms, 3]. Contains new atomic coordinates.
   \param n_atoms number of atoms. Implementations may ignore this parameter if number if atoms is known at invocation moment.
 */
@@ -71,7 +71,7 @@ extern "C" void ASI_set_atom_coords(const double *coords, int n_atoms = -1);
   Set coordinates of atoms and lattice vectors
   
   Set coordinates of atoms and lattice vectors. Same as ASI_set_atom_coords() but with additional
-  parameter for lattice vectors.
+  parameter for lattice vectors. Coordinates and lattive vectors are copied into interal storage, so the pointer do not have to be valid after return.
   
   \param coords pointer to array in row major format with shape [n_atoms, 3]. Contains new atomic coordinates.
   \param n_atoms number of atoms. Implementations may ignore this parameter if number if atoms is known at invocation moment.
@@ -108,25 +108,25 @@ extern "C" void ASI_register_external_potential(ASI_ext_pot_func_t callback, voi
 extern "C" void ASI_run();
 
 /*!
-  Return number of atoms
+  Returns number of atoms
   
-  Return number of atoms
+  Returns number of atoms
   \return number of atoms
 */
 extern "C" int ASI_n_atoms();
 
 /*!
-  Return total energy
+  Returns total energy
   
-  Return total energy
+  Returns total energy
   \return total energy
 */
 extern "C" double ASI_energy();
 
 /*!
-  Return forces acting on each atom
+  Returns forces acting on each atom
   
-  Return forces acting on each atom. May return `NULL` if forces calculation was not configured on ASI_init() call.
+  Returns forces acting on each atom. May return `NULL` if forces calculation was not configured on ASI_init() call.
   For FHI-aims use `compute_forces .true.`. For DFTB+ use `Analysis { CalculateForces = Yes }`.
   
   \return pointer to array in row major format of shape `[ASI_n_atoms(), 3]` or `NULL`.
@@ -159,17 +159,117 @@ extern "C" const double* ASI_atomic_charges(int scheme=-1);
 */
 extern "C" void ASI_calc_esp(int n, const double *coords, double *potential, double *potential_grad);
 
+/*!
+  Returns number of spin channels.
+  
+  Returns number of spin channels. Returns 1 for spin-paired calculations.
+  
+  \return number of spin channels. Returns 1 for spin-paired calculations.
+*/
 extern "C" int ASI_get_nspin();
-extern "C" int ASI_get_nkpts();
-extern "C" int ASI_get_n_local_ks();
-extern "C" int ASI_get_local_ks(int *local_ks); // local_ks[0, i] == i_kpnt, local_ks[1, i] == i_spin; i < ASI_get_n_local_ks();  iK and iS are 1-based indices
-extern "C" int ASI_get_basis_size();
-extern "C" bool ASI_is_hamiltonian_real();
-extern "C" void ASI_register_dm_callback(ASI_dmhs_callback_t , void *aux_ptr); // AIMS: `density_update_method density_matrix` option is necessary; cannot be used if forces are computed (returns garbage)
-extern "C" void ASI_register_overlap_callback(ASI_dmhs_callback_t , void *aux_ptr);
-extern "C" void ASI_register_hamiltonian_callback(ASI_dmhs_callback_t , void *aux_ptr);
-extern "C" void ASI_register_dm_init_callback(ASI_dmhs_callback_t , void *aux_ptr); // AIMS: elsi_restart read is necessary
 
+/*!
+  Returns number of k-points.
+  
+  Returns number of k-points. Returns 1 for non-periodic or Gamma-point calculations.
+  
+  \return number of k-points. Returns 1 for non-periodic or Gamma-point calculations.
+*/
+extern "C" int ASI_get_nkpts();
+
+/*!
+  Returns number of (k-point, spin channel) pairs processed by current process.
+  
+  Meant to be used in case of distributed (MPI) calculation, when parallelization is performed along k-points 
+  and spin channels. Returns number of (k-point, spin channel) pairs processed by current process.
+  
+  \return number of (k-point, spin channel) pairs processed by current process.
+*/
+extern "C" int ASI_get_n_local_ks();
+
+/*!
+  Returns list of (k-point, spin channel) pairs processed by current process.
+  
+  Meant to be used in case of distributed (MPI) calculation, when parallelization is performed along k-points 
+  and spin channels. Returns list and number of (k-point, spin channel) pairs processed by current process.
+  
+  \param pointer to output array 
+  \return number of (k-point, spin channel) pairs processed by current process.
+*/
+extern "C" int ASI_get_local_ks(int *local_ks); // local_ks[0, i] == i_kpnt, local_ks[1, i] == i_spin; i < ASI_get_n_local_ks();  iK and iS are 1-based indices
+
+/*!
+  Returns basis set size
+  
+  Returns basis set size. Defines total size of DM, H, and S matrices passed into callbacks of \ref ASI_dmhs_callback_t type.
+  Note, that in case of distributed calculation local matrix size is defined by BLACS descriptor.
+  \return basis set size. 
+*/
+extern "C" int ASI_get_basis_size();
+
+/*!
+  If hamiltonias, overlap, and density matrices are real.
+  
+  Returns True if hamiltonias, overlap, and density matrices are real. Returns False otherwise. 
+  It defines type of matrices passed into callbacks of \ref ASI_dmhs_callback_t type.
+
+  \return if hamiltonias, overlap, and density matrices are real.
+*/
+extern "C" bool ASI_is_hamiltonian_real();
+
+/*!
+  Register callback for density matrix evaluation.
+  
+  Register callback for density matrix evaluation. 
+  Callback will be invoked on each density matrix evaluation, including evaluations within SCF loop.
+  Note, that FHI-aims evaulates density matrix in SCF loop only if option `density_update_method density_matrix` is in config file.
+  Note, that if FHI-aims evaulates forces then the last evaluations of the density matrix do not correspond to the calculated geometry.
+  
+  \param callback callback of type \ref ASI_dmhs_callback_t
+  \param aux_ptr auxilary pointer that will be passed in every `callback` call.
+*/
+extern "C" void ASI_register_dm_callback(ASI_dmhs_callback_t callback, void *aux_ptr);
+
+/*!
+  Register callback for overlap matrix evaluation.
+  
+  Register callback for overlap matrix evaluation. 
+  Callback will be invoked on each overlap matrix evaluation. Although overlap matrix is the same for all k-point and spin channels, 
+  the callback will be invoked for each (k-point, spin channel) pair. It is up to client to decide when is the best time to work with the matrix. 
+  Computational cost of callback invocation is negligible if no work is done in callback itself.
+  
+  \param callback callback of type \ref ASI_dmhs_callback_t
+  \param aux_ptr auxilary pointer that will be passed in every `callback` call.
+*/
+extern "C" void ASI_register_overlap_callback(ASI_dmhs_callback_t , void *aux_ptr);
+
+/*!
+  Register callback for hamiltonian matrix evaluation.
+  
+  Register callback for hamiltonian matrix evaluation. 
+  Callback will be invoked on each hamiltonian matrix evaluation.
+  
+  \param callback callback of type \ref ASI_dmhs_callback_t
+  \param aux_ptr auxilary pointer that will be passed in every `callback` call.
+*/
+extern "C" void ASI_register_hamiltonian_callback(ASI_dmhs_callback_t , void *aux_ptr);
+
+/*!
+  Register callback for density matrix initial guess.
+  
+  Register callback for density matrix initial guess. Callback is expected to fill the matrix argument with initial guess of the density matrix.
+  It is a good place for calculation restart or density matrix prediction.
+  
+  \param callback callback of type \ref ASI_dmhs_callback_t
+  \param aux_ptr auxilary pointer that will be passed in every `callback` call.
+*/
+extern "C" void ASI_register_dm_init_callback(ASI_dmhs_callback_t , void *aux_ptr);
+
+/*!
+  Finalize calculation.
+  
+  Free memory, close openned files. Library may be unloaded after that. No API calls are permited after that call.
+*/
 extern "C" void ASI_finalize();
 // no calls after ASI_finalize
 
